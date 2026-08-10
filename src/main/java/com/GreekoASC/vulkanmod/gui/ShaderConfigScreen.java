ed0d37e93db12b047449e2df849114c5258e2a72
GreekoASC <greeko2030@gmail.com>
2026-08-10 21:37:12 +0600

    Update ShaderConfigScreen.java

--- src/main/java/com/GreekoASC/vulkanmod/gui/ShaderConfigScreen.java
+++ src/main/java/com/GreekoASC/vulkanmod/gui/ShaderConfigScreen.java
@@ -1,1 +1,20 @@
+package com.yourname.vulkanmod.config;
+
+public class ShaderSettings {
+    // Boolean Toggles
+    public boolean volumetricClouds = true;
+    public boolean waterCaustics = true;
+    public boolean softDynamicShadows = true;
+    public boolean rainAtmosphere = true;
+    
+    // Slider Values (Float)
+    public float eyeAdaptationIntensity = 0.5f; // Range: 0.0 to 1.0
+    public float colorTemperature = 6500.0f;    // Range: 1000.0 to 12000.0 (Kelvin)
+
+    private static ShaderSettings instance = new ShaderSettings();
+
+    public static ShaderSettings getInstance() {
+        return instance;
+    }
+}
 
