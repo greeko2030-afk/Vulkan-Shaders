f4cc0e2ff72525cd85df73cb6445dab4963353c6
GreekoASC <greeko2030@gmail.com>
2026-08-10 21:39:43 +0600

    Update VulkanUniformManager.java

--- src/main/java/com/GreekoASC/vulkanmod/shader/VulkanUniformManager.java
+++ src/main/java/com/GreekoASC/vulkanmod/shader/VulkanUniformManager.java
@@ -1,1 +1,27 @@
+package com.com.greekoname.vulkanmod.shader;
+
+import com.greeko.vulkanmod.config.ShaderSettings;
+
+public class VulkanUniformManager {
+    
+    public static void updateUniforms() {
+        ShaderSettings config = ShaderSettings.getInstance();
+        
+        // Booleans to Integers
+        int cloudsFlag = config.volumetricClouds ? 1 : 0;
+        int causticsFlag = config.waterCaustics ? 1 : 0;
+        
+        // Floats
+        float eyeAdaptation = config.eyeAdaptationIntensity;
+        float colorTemp = config.colorTemperature;
+        
+        // Send to GPU Pipeline
+        sendToVulkanPipeline(cloudsFlag, causticsFlag, eyeAdaptation, colorTemp);
+    }
+
+    private static void sendToVulkanPipeline(int clouds, int caustics, float eyeAdaptation, float colorTemp) {
+        // LWJGL3 Vulkan bindings to push these values to the GPU (VkPushConstantRange)
+        // Example: vkCmdPushConstants(commandBuffer, pipelineLayout, VK_SHADER_STAGE_FRAGMENT_BIT, 0, bufferData);
+    }
+}
 
