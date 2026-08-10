0e0d0b54605b1118af36d5b3812f06ae614b38d9
GreekoASC <greeko2030@gmail.com>
2026-08-10 21:44:40 +0600

    Update VulkanModClient.java

--- src/main/java/com/GreekoASC/vulkanmod/VulkanModClient.java
+++ src/main/java/com/GreekoASC/vulkanmod/VulkanModClient.java
@@ -1,1 +1,20 @@
+package com.com.greeko.vulkanmod;
+
+import com.com.greeko.vulkanmod.shader.ShaderManager;
+import net.fabricmc.api.ClientModInitializer;
+
+public class VulkanModClient implements ClientModInitializer {
+    
+    @Override
+    public void onInitializeClient() {
+        // Extract default shaders when the client starts
+        ShaderManager.initDefaultShader();
+        
+        // Note: You would also register your Keybindings and Command registrations here
+        // VulkanModClientKeys.registerKeys();
+        // VulkanShaderErrorCommand.register();
+        
+        System.out.println("[VulkanMod] Client Initialized - Vulkan SPIR-V Shader support ready!");
+    }
+}
 
