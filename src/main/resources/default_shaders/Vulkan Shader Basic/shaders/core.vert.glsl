930555a16cabca3d740cebdb300376d856932820
GreekoASC <greeko2030@gmail.com>
2026-08-10 21:46:25 +0600

    Update core.vert.glsl

--- src/main/resources/default_shaders/Vulkan Shader Basic/shaders/core.vert.glsl
+++ src/main/resources/default_shaders/Vulkan Shader Basic/shaders/core.vert.glsl
@@ -1,1 +1,29 @@
+#version 450
 
+// Input attributes from Minecraft vertex buffer
+layout(location = 0) in vec3 inPosition;
+layout(location = 1) in vec2 inUV;
+layout(location = 2) in vec3 inNormal;
+
+// Push constants or uniform buffers for Matrix transformations
+layout(push_constant) uniform Matrices {
+    mat4 modelViewMatrix;
+    mat4 projectionMatrix;
+} matrices;
+
+// Output variables sent to the Fragment Shader (core.frag.glsl)
+layout(location = 0) out vec2 outUV;
+layout(location = 1) out vec3 outNormal;
+layout(location = 2) out vec3 outWorldPos;
+
+void main() {
+    // Pass data to fragment shader
+    outUV = inUV;
+    outNormal = inNormal;
+    
+    // Simplified world position calculation
+    outWorldPos = inPosition; 
+    
+    // Calculate final screen position of the vertex
+    gl_Position = matrices.projectionMatrix * matrices.modelViewMatrix * vec4(inPosition, 1.0);
+}
